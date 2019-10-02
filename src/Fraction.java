6dd677f5ba69b23ff599ec29e3855bb3c1db3ac3
RachData <rachidderra@gmail.com>
2019-10-02 08:47:04 +0100

    Ajoutez un constructeur et la méthode toString à la classe Fraction et modifiez la classe Main

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,4 +1,26 @@
 
 public class Fraction {
+	
+	private int numerateur; 
+    private int denominateur;
 
+	/**
+	 * Constructeur d'un nombre rationnel
+     * 2 paramètres entiers
+     * @param numerateur
+     * @param denominateur
+	 */
+	public Fraction(int numera, int denomina){
+		numerateur=numera;
+		denominateur=denomina;
+		
+	}
+	
+	/**
+	 * Methode de presentation
+	 */
+	public String toString(){
+		
+		return "numerateur: "+numerateur+" et comme denominateur: "+denominateur;
+	}
 }
--- src/Principale.java
+++ src/Principale.java
@@ -3,7 +3,9 @@ public class Principale {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Word");
+		int numerateur=8,denominateur=2;
+		Fraction fraction1= new Fraction(numerateur,denominateur);
+		System.out.println("l'objet Fraction1 contient comme "+ fraction1.toString());
 
 	}
 
